55800094000ed3c5b68519ce06451fd3023e864f
Braden Farmer <farmerbb@gmail.com>
2024-09-11 11:42:07 -0600

    Cleanup commented out code

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,6 @@ allprojects {
     }
 }
 
-// tasks.register("clean", Delete::class) {
-//     delete(rootProject.buildDir)
-// }
-
 plugins {
     id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
